209cace634c724278a79bd9cac2bab3382caaf36
TreNgheDiCode <gabayan170@gmail.com>
2023-07-20 03:23:04 +0700

    Clerk Authentication

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,7 +1,22 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 const LandingPage = () => {
     return (
         <div>
             Landing Page (Unprotected)
+            <div>
+                <Link href="/sign-in">
+                    <Button>
+                        Đăng nhập
+                    </Button>
+                </Link>
+                <Link href="/sign-up">
+                    <Button>
+                        Đăng ký
+                    </Button>
+                </Link>
+            </div>
         </div>
     )
 }
