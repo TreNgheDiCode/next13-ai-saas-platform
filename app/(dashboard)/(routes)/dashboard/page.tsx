91dbcce140df2a063915aa03b354637d45e39cb6
TreNgheDiCode <gabayan170@gmail.com>
2023-07-20 04:25:56 +0700

    Sidebar

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -3,8 +3,7 @@ import { UserButton } from "@clerk/nextjs";
 const DashboardPage = () => {
   return (
     <div>
-      <p>Dashboard Page (Protected)</p>
-      <UserButton afterSignOutUrl="/"/>
+      <p>Trang điều khiển (Bảo vệ)</p>
     </div>
   )
 }
