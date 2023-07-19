d1e844ae70823461d2b781f434635edf68fe8f22
TreNgheDiCode <gabayan170@gmail.com>
2023-07-20 05:03:55 +0700

    Dashboard

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -6,6 +6,7 @@ import { Settings, Code, Music, VideoIcon, ImageIcon, LayoutDashboard, MessageSq
 import { Montserrat } from "next/font/google";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const montserrat = Montserrat({ weight: "600", subsets: ["latin"] })
 
@@ -54,20 +55,23 @@ const routes = [
 ]
 
 const Sidebar = () => {
-  return ( 
-    <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
+	const pathName = usePathname();
+	return ( 
+		<div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
 			<div className="px-3 py-2 flex-1">
 				<Link href="/dashboard" className="flex items-center pl-3 mb-14">
-          <div className="relative w-8 h-8 mr-4">
-            <Image fill alt="Logo" src="/Logo.png"/>
-          </div>
+					<div className="relative w-8 h-8 mr-4">
+						<Image fill alt="Logo" src="/Logo.png"/>
+					</div>
 					<h1 className={cn( "text-2xl font-bold", montserrat.className)}>
 						CIGP Platform
 					</h1>
 				</Link>
 				<div className="space-y-1">
 					{routes.map((route) => (
-						<Link href={route.href} key={route.href} className="text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition">
+						<Link href={route.href} key={route.href} className={cn("text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition",
+						pathName == route.href ? "text-white bg-white/10" : "text-zinc-400"
+						)}>
 							<div className="flex items-center flex-1">
 								<route.icon className={cn("h-5 w-5 mr-3", route.color)}/>
 								{route.label}
@@ -75,9 +79,9 @@ const Sidebar = () => {
 						</Link>
 					))}
 				</div>
-      </div>
+			</div>
 		</div>
-    );
+	);
 }
 
 export default Sidebar;
