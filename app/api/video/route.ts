fbf0c15fdb33e67124cd8261ad1729c6ac5333a0
TreNgheDiCode <gabayan170@gmail.com>
2023-09-08 04:25:46 +0700

    Video Generation AI

---
+++ app/api/video/route.ts
@@ -0,0 +1,38 @@
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+import Replicate from "replicate";
+
+const replicate = new Replicate({
+  auth: process.env.REPLICATE_API_KEY!,
+});
+
+export async function POST(req: Request) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+    const { prompt } = body;
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    if (!prompt) {
+      return new NextResponse("Prompt is required", { status: 400 });
+    }
+
+    const response = await replicate.run(
+      "deforum/deforum_stable_diffusion:e22e77495f2fb83c34d5fae2ad8ab63c0a87b6b573b6208e1535b23b89ea66d6",
+      {
+        input: {
+          animation_prompts: prompt,
+          max_frames: 200,
+        },
+      }
+    );
+
+    return NextResponse.json(response);
+  } catch (error) {
+    console.log("[VIDEO_ERROR]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
